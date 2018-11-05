fc2e9cee0f32f378164feda19821d214e7669f8d
lzl <294523194@qq.com>
2018-11-05 17:51:03 +0800

    合5s

--- src/main/java/com/example/vr/MainActivity.java
+++ src/main/java/com/example/vr/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
         List<User> usersList=userDatabase.getUserDao().getAllUsers();
         for (User users:usersList){
-            Log.e(TAG,"姓名：++++++333333"+users.getName()+" 年龄："+users.getAge());
+            Log.e(TAG,"姓名：++++++333333333"+users.getName()+" 年龄："+users.getAge());
         }
 
         User user3=userDatabase.getUserDao().getUser(2);
